5e532ca57eaa2e577c4fd37696a26db8cfb329f9
DataCollectApp <datacollectapp@jarand.dev>
2020-01-05 22:01:15 +0100

    Added retention and target to annotation

--- src/main/java/app/datacollect/lastread/EnableLastRead.java
+++ src/main/java/app/datacollect/lastread/EnableLastRead.java
@@ -2,7 +2,13 @@ package app.datacollect.lastread;
 
 import app.datacollect.lastread.repository.LastReadRepository;
 import app.datacollect.lastread.service.LastReadService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.context.annotation.Import;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 @Import({LastReadService.class, LastReadRepository.class})
 public @interface EnableLastRead {}
